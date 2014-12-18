aba65f62c8e722fbb1f2bcc8016368280283027c
anestis27 <christosanestis.georgiadis@gmail.com>
2014-12-18 23:47:23 +0200

    HomeActivity UserdetailsActivity

--- app/src/main/java/com/example/vromia/e_nurseproject/Activities/UserDetailsActivity.java
+++ app/src/main/java/com/example/vromia/e_nurseproject/Activities/UserDetailsActivity.java
@@ -1,15 +1,78 @@
 package com.example.vromia.e_nurseproject.Activities;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.example.vromia.e_nurseproject.R;
+
 
 /**
  * Created by Vromia on 17/12/2014.
  */
 public class UserDetailsActivity extends Activity {
 
+    private EditText onoma, ilikia, ypsos, baros, istorikoPathiseon;
+    private RadioGroup fylo;
+    private Button btBack, btOk;
+    private RadioButton rb_male, rb_female;
+    private String Sfylo="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+        initUI();
+        initListeners();
+    }
+
+    private void initUI() {
+        onoma = (EditText) findViewById(R.id.onoma);
+        ilikia = (EditText) findViewById(R.id.ilikia);
+        ypsos = (EditText) findViewById(R.id.ypsos);
+        baros = (EditText) findViewById(R.id.baros);
+        istorikoPathiseon = (EditText) findViewById(R.id.istorikoPathiseon);
+        fylo = (RadioGroup) findViewById(R.id.fylo);
+        btBack = (Button) findViewById(R.id.btBack);
+        btOk = (Button) findViewById(R.id.btOk);
+        rb_male = (RadioButton) findViewById(R.id.rb_male);
+        rb_female = (RadioButton) findViewById(R.id.rb_female);
+    }
+
+    private void initListeners() {
+        fylo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // TODO Auto-generated method stub
+                if (rb_male.isChecked()) {
+                    Sfylo="Άνδρας";
+                } else if (rb_female.isChecked()) {
+                    Sfylo="Γυναίκα";
+                }
+            }
+        });
+
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //edo tha pigainei sto home
+            }
+        });
+
+        btOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Sonoma, Silikia, Sypsos, Sbaros, SistorikoPathiseon;
+                Sonoma = String.valueOf(onoma.getText());
+                Silikia = String.valueOf(ilikia.getText());
+                Sypsos = String.valueOf(ypsos.getText());
+                Sbaros = String.valueOf(baros.getText());
+                SistorikoPathiseon = String.valueOf(istorikoPathiseon.getText());
+            }
+        });
     }
 }
